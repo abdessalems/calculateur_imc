f25230c1c3eda224d4290d7514d763548ada825f
SAADAOUI ABDESSALEM <107399601+abdessalems@users.noreply.github.com>
2024-01-31 17:30:27 +0100

    Delete TTDPaysFoodWithJava/TTDPaysFoodWithJava directory

--- TTDPaysFoodWithJava/TTDPaysFoodWithJava/src/main/java/saadaoui/ttdpaysfoodwithjava/repos/CountryRepository.java
+++
@@ -1,7 +0,0 @@
-package saadaoui.ttdpaysfoodwithjava.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import saadaoui.ttdpaysfoodwithjava.entity.Country;
-
-public interface CountryRepository extends JpaRepository<Country, Long> {
-}
