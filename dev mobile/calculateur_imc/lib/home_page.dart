edd9cf5f95947d92db75d0658077c1507d2964ef
Baganda49 <abdessalemsaa@gmail.com>
2024-02-07 18:52:57 +0100

    add function of display list of users

--- dev mobile/calculateur_imc/lib/home_page.dart
+++ dev mobile/calculateur_imc/lib/home_page.dart
@@ -1,7 +1,7 @@
-import 'package:calculateur_imc/bmi_calculator.dart';
 import 'package:flutter/material.dart';
 import 'package:calculateur_imc/api_service.dart';
 import 'package:calculateur_imc/user.dart';
+import 'package:calculateur_imc/bmi_calculator.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -21,15 +21,11 @@ class _HomePageState extends State<HomePage> {
 
   Future<void> _loadUsers() async {
     try {
-      // Call the API service to get the list of users
       List<User> users = await _apiService.getUsers();
-      
-      // Update the local list of users
       setState(() {
         _users = users;
       });
     } catch (e) {
-      // Handle error
       print('Error loading users: $e');
     }
   }
@@ -37,10 +33,8 @@ class _HomePageState extends State<HomePage> {
   Future<void> _addUser(User user) async {
     try {
       await _apiService.createUser(user);
-      // Reload users after adding
       _loadUsers();
     } catch (e) {
-      // Handle error
       print('Error: $e');
     }
   }
@@ -48,13 +42,23 @@ class _HomePageState extends State<HomePage> {
   Future<void> _updateUser(User user) async {
     try {
       await _apiService.updateUser(user);
-      // Reload users after updating
       _loadUsers();
     } catch (e) {
-      // Handle error
       print('Error: $e');
     }
   }
+
+  Future<void> _deleteUser(int id) async {
+  try {
+    await _apiService.deleteUser(id);
+    setState(() {
+      _users.removeWhere((user) => user.id == id);
+    });
+  } catch (e) {
+    // Handle error
+    print('Error deleting user: $e');
+  }
+}
 
 
   @override
@@ -74,7 +78,7 @@ class _HomePageState extends State<HomePage> {
                 Text('Age: ${_users[index].age}'),
                 Text('Height: ${_users[index].height} cm'),
                 Text('Weight: ${_users[index].weight} kg'),
-                Text('BMI: ${_calculateBMI(_users[index]) ?? 'N/A'}'), // Display BMI
+                Text('BMI: ${_calculateBMI(_users[index]) ?? 'N/A'}'),
               ],
             ),
             trailing: Row(
@@ -83,13 +87,13 @@ class _HomePageState extends State<HomePage> {
                 IconButton(
                   icon: Icon(Icons.edit),
                   onPressed: () {
-                    _editUser(_users[index]); // Edit user when edit button is pressed
+                    _editUser(_users[index]);
                   },
                 ),
                 IconButton(
                   icon: Icon(Icons.delete),
                   onPressed: () {
-                    _deleteUser(_users[index].id); // Delete user when delete button is pressed
+                    _deleteUser(_users[index].id);
                   },
                 ),
               ],
@@ -99,16 +103,15 @@ class _HomePageState extends State<HomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          _addUserDialog(); // Show dialog to add a new user when floating action button is pressed
+          _addUserDialog();
         },
         child: Icon(Icons.add),
       ),
     );
   }
 
-  // Method to show dialog for adding a new user
   Future<void> _addUserDialog() async {
-    User newUser = User(id: 0, name: '', age: 0, height: 0, weight: 0); // Initialize a new user
+    User newUser = User(id: 0, name: '', age: 0, height: 0, weight: 0);
     await showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -149,14 +152,14 @@ class _HomePageState extends State<HomePage> {
           actions: <Widget>[
             TextButton(
               onPressed: () {
-                Navigator.of(context).pop(); // Close the dialog
+                Navigator.of(context).pop();
               },
               child: Text('Cancel'),
             ),
             TextButton(
               onPressed: () {
-                _addUser(newUser); // Add the user to the list
-                Navigator.of(context).pop(); // Close the dialog
+                _addUser(newUser);
+                Navigator.of(context).pop();
               },
               child: Text('Add'),
             ),
@@ -166,79 +169,83 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  // Method to calculate BMI for a user
   double? _calculateBMI(User user) {
     if (user.height != null && user.weight != null) {
       return BMICalculator.calculateBMI(user.weight!, user.height!);
     }
-    return null; // Return null if height or weight is null
+    return null;
   }
 
-  // Method to show dialog for editing an existing user
-
-  Future<void> _editUser(User user) async {
-    User editedUser = user; // Initialize with the existing user's details
-    await showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Edit User'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                decoration: InputDecoration(labelText: 'Name'),
-                controller: TextEditingController(text: editedUser.name),
-                onChanged: (value) {
-                  setState(() {
-                    editedUser.name = value;
-                  });
-                },
-              ),
-              TextField(
-                decoration: InputDecoration(labelText: 'Age'),
-                keyboardType: TextInputType.number,
-                controller: TextEditingController(text: editedUser.age.toString()),
-                onChanged: (value) {
-                  setState(() {
-                    editedUser.age = int.parse(value);
-                  });
-                },
-              ),
-            ],
-          ),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(); // Close the dialog
+ 
+ Future<void> _editUser(User user) async {
+  User editedUser = user;
+  await showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Edit User'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Name'),
+              controller: TextEditingController(text: editedUser.name),
+              onChanged: (value) {
+                setState(() {
+                  editedUser.name = value;
+                });
               },
-              child: Text('Cancel'),
             ),
-            TextButton(
-              onPressed: () {
-                _updateUser(editedUser); // Update the user in the list
-                Navigator.of(context).pop(); // Close the dialog
+            TextField(
+              decoration: InputDecoration(labelText: 'Age'),
+              keyboardType: TextInputType.number,
+              controller: TextEditingController(text: editedUser.age.toString()),
+              onChanged: (value) {
+                setState(() {
+                  editedUser.age = int.parse(value);
+                });
               },
-              child: Text('Save'),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Height'),
+              keyboardType: TextInputType.number,
+              controller: TextEditingController(text: editedUser.height.toString()),
+              onChanged: (value) {
+                setState(() {
+                  editedUser.height = double.parse(value);
+                });
+              },
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Weight'),
+              keyboardType: TextInputType.number,
+              controller: TextEditingController(text: editedUser.weight.toString()),
+              onChanged: (value) {
+                setState(() {
+                  editedUser.weight = double.parse(value);
+                });
+              },
             ),
           ],
-        );
-      },
-    );
-  }
-
-  Future<void> _deleteUser(int id) async {
-    try {
-      await _apiService.deleteUser(id);
-      setState(() {
-        _users.removeWhere((user) => user.id == id);
-      });
-    } catch (e) {
-      // Handle error
-      print('Error: $e');
-    }
-  }
-
-
+        ),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () {
+              _updateUser(editedUser);
+              Navigator.of(context).pop();
+            },
+            child: Text('Save'),
+          ),
+        ],
+      );
+    },
+  );
+}
 
 }
